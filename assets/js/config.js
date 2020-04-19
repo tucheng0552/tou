2a0c662ecea159ac58b61f6ef21b9f94e444e72c
tucheng0552 <35320073+tucheng0552@users.noreply.github.com>
2020-04-19 23:50:30 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,19 +3,19 @@
  * */
 
 const config = {
-  // API_URL:'https://api.tou.duoduodp.com',
+  // API_URL:'',
   // API_URL_TEST: 'http://120.79.165.253:8099',
   // API_URL_TEST:'http://192.168.1.14:8091',
   API_URL_TEST:'http://gongsi.sztou.cn:8091',
 
-  WEB_ASSETS_URL:'https://web.tou.duoduodp.com/zh',
-  DP_ASSETS_URL:'https://www.duoduodp.com',
+  WEB_ASSETS_URL:'',
+  DP_ASSETS_URL:'',
 
-  ALI_PAY_URL: 'https://openapi.alipaydev.com/gateway.do?charset=utf-8',
-  WE_CHAT_PAY_URL: 'https://api.mch.weixin.qq.com/pay/unifiedorder',
+  ALI_PAY_URL: '',
+  WE_CHAT_PAY_URL: '',
 
-  AES_KEY: 'ff16b1fbb9e648aa',
-  AES_iv: 'mm16c1fbb9e648aa'
+  AES_KEY: '',
+  AES_iv: ''
 };
 
 export default config
